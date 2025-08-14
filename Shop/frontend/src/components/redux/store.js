df3ee4e15f75d50f35624f5847e855894c4a48cd
Amir20Hossein <13gameplay83@gmail.com>
2025-08-14 14:06:21 +0330

    .

--- Shop/frontend/src/components/redux/store.js
+++ Shop/frontend/src/components/redux/store.js
@@ -1,2 +1,13 @@
 import {creatStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk'
+import categoriesReducer from '../reducers/categoriesReducers';
+import productsReducer from '../reducers/productsReducers';
+
+const rootReducer = combineReducers({
+    categories: categoriesReducer,
+    products: productsReducer
+});
+
+const store = configureStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
