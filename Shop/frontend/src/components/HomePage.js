be81c57d0ab8625c188372424cf14863d6e7168f
Amir20Hossein <13gameplay83@gmail.com>
2025-08-18 01:55:37 +0330

    .

--- Shop/frontend/src/components/HomePage.js
+++ Shop/frontend/src/components/HomePage.js
@@ -3,8 +3,8 @@ import { useDispatch , useSelector } from 'react-redux';
 import {fetchCategories} from '../redux/actions/categoriesActions';
 import {fetchProducts} from '../redux/actions/productsActions';
 import CategoryList from '../components/CategoryList';
-import FlashSales from '../components/FlashSales';
-import TopSelling from '../components/TopSelling';
+import FlashSales from '../components/FlashSalesProducts';
+import TopSelling from '../components/TopSellingProducts';
 import CategoryProducts from '../components/CategoryProducts';
 
 const HomePage = () => {
@@ -20,10 +20,29 @@ const HomePage = () => {
 
     return (
         <div>
-            <CategoryList categories={categories} />
-            <FlashSales products={products} />
-            <TopSelling products={products} />
-            <CategoryProducts products={products} />
+            {/*start row for categories and flash sales*/}
+            <div className="row">
+                <div className="col-md-3">
+                    <CategoryList categories={categories} />
+                </div>
+                <div className="col-md-9">
+                    <FlashSalesProducts products={categories} />
+                </div>
+            </div>
+            {/*Second row row for top selling and category products*/}
+            
+            <div className="row">
+                <TopSellingProducts products={products} />
+            </div>
+            
+            {/* third forth and fith rows for products */}
+            {categories.map(category => (
+                <div key={category.id} className="row">
+                    <CategoryProducts category={category} products={products}/>
+                </div>
+            ))}
+            
         </div>
     );
-}
+};
+export default HomePage;
