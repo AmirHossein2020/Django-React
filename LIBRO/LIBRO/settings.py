758d7b37f5823141313105ad4e53dd1a00226724
Amir20Hossein <13gameplay83@gmail.com>
2025-12-04 12:46:05 +0330

    .

--- LIBRO/LIBRO/settings.py
+++ LIBRO/LIBRO/settings.py
@@ -83,10 +83,16 @@ WSGI_APPLICATION = 'LIBRO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
 
 
 # Password validation
